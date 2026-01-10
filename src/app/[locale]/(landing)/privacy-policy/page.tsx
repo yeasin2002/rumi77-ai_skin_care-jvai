58d451ac4986592877ff45691edaec960600f661
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-01-10 15:42:21 +0600

    feat: created Privacy Policy & Privacy Policy page

---
+++ src/app/[locale]/(landing)/privacy-policy/page.tsx
@@ -0,0 +1,30 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className="bg-background min-h-screen px-6 py-20 lg:px-8">
+      <div className="px-32">
+        <h1 className="font-caudex text-main-button mb-10 text-center text-7xl leading-none font-normal tracking-normal">
+          Privacy Policy
+        </h1>
+
+        <p className="font-open-sans text-main-button text-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
