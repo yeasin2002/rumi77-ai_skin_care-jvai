df7342232b92d1e4f57954b24ea0fa71500acb81
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-01-10 10:34:05 +0600

    feat: created initial auth files

---
+++ src/app/[locale]/verify-otp/page.tsx
@@ -0,0 +1,5 @@
+const Verify_OTP = () => {
+  return <div>Verify_OTP</div>
+}
+
+export default Verify_OTP
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,8 @@
 export * from './button'
+export * from './card'
+export * from './carousel'
 export * from './dropdown-menu'
+export * from './input'
+export * from './label'
+export * from './sheet'
+
