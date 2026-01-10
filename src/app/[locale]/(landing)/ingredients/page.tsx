8dbb4a20e444cba9d48d11164eda4535623f1d4a
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-01-10 14:48:16 +0600

    fix: Ingredients page design issues

--- src/app/[locale]/(landing)/ingredients/page.tsx
+++ src/app/[locale]/(landing)/ingredients/page.tsx
@@ -1,31 +1,30 @@
 import ingredientsBg from '@/assets/image/ingredients-bg.jpg'
 import { NavList } from '@/components/shared'
-import { Footer } from '@/components/shared/footer'
 import Image from 'next/image'
 
 const Ingredients = () => {
   return (
-    <div>
-      <main className="relative">
-        <div>
-          <Image
-            src={ingredientsBg}
-            alt="Background"
-            className="z-50 h-screen w-full object-cover"
-          />
-
-          <h1 className="font-caudex text-main-button absolute bottom-10 left-20 text-5xl leading-none font-normal tracking-normal">
-            Timeless Glow, Science- <br /> Backed Skincare
-          </h1>
-        </div>
+    <main>
+      <div className="relative">
+        <Image
+          src={ingredientsBg}
+          alt="Background"
+          className="z-50 h-screen w-full object-cover"
+          priority
+          quality={100}
+        />
+        <h1 className="font-caudex text-main-button absolute bottom-10 left-20 text-5xl leading-none font-normal tracking-normal">
+          Timeless Glow, Science- <br /> Backed Skincare
+        </h1>
 
         <NavList
           wrapperClassName="absolute top-5 right-2 z-20 justify-around md:w-full"
           className="text-black"
         />
-      </main>
-      <Footer className="px-20!" />
-    </div>
+      </div>
+
+      <div className="bg-main-primary-base_medium mt-32 h-0.5"></div>
+    </main>
   )
 }
 
