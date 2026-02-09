5f591fc4fdb48237b6a115960797b47c843514ed
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-09 16:40:22 +0600

    fix: modal showing issue

--- src/app/[locale]/(landing)/(home)/show-modals.tsx
+++ src/app/[locale]/(landing)/(home)/show-modals.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useSeasonStorage } from '@/hooks/useSeasonStorage'
-import { useEffect, useState } from 'react'
+import { useSessionStorage } from '@/hooks/useSessionStorage'
+import { useState } from 'react'
 import { BeMemberContent } from './Dialog/be-member-content'
 import { CreateAccountContent } from './Dialog/create-account-content'
 import { ModalContainer } from './Dialog/modal-container'
@@ -9,37 +9,17 @@ import { WelcomeContent } from './Dialog/welcome-content'
 
 export type ModalStep = 'be-member' | 'create-account' | 'welcome' | null
 
-const getSeasonName = (monthIndex: number) => {
-  if (monthIndex >= 2 && monthIndex <= 4) return 'spring'
-  if (monthIndex >= 5 && monthIndex <= 7) return 'summer'
-  if (monthIndex >= 8 && monthIndex <= 10) return 'autumn'
-  return 'winter'
-}
-
 export const ShowModals = () => {
-  const [currentStep, setCurrentStep] = useState<ModalStep>(null)
-  const { get, set } = useSeasonStorage('modal')
+  const [hasClosedModal, setHasClosedModal] = useSessionStorage('modal-closed', false)
 
-  const now = new Date()
-  const seasonKey = `home-dismissed-${now.getFullYear()}-${getSeasonName(now.getMonth())}`
-
-  // Show the modal only if it was not dismissed in the current season.
-  useEffect(() => {
-    const isDismissedForSeason = get<boolean>(seasonKey, false)
-    if (isDismissedForSeason) return
-
-    const timer = setTimeout(() => {
-      setCurrentStep('be-member')
-    }, 1000) // 1 seconds
-
-    return () => clearTimeout(timer)
-  }, [get, seasonKey])
+  // Initialize modal state based on session storage
+  const [currentStep, setCurrentStep] = useState<ModalStep>(() =>
+    hasClosedModal ? null : 'be-member'
+  )
 
   const handleClose = () => {
-    if (currentStep !== null) {
-      set(seasonKey, true)
-    }
     setCurrentStep(null)
+    setHasClosedModal(true)
   }
 
   const handleJoinClick = () => {
--- src/app/[locale]/dashboard/dashboard-guards.tsx
+++ src/app/[locale]/dashboard/dashboard-guards.tsx
@@ -4,9 +4,10 @@ import { useAuthStore } from '@/store/auth.store'
 import { useRouter } from 'next/navigation'
 
 export const DashboardGuards = ({ children }: { children: React.ReactNode }) => {
-  const token = useAuthStore((state) => state.token)
-  console.log('🚀 ~ DashboardGuards ~ token:', token)
   const router = useRouter()
+  const token = useAuthStore((state) => state.token)
+
+  if (typeof window === 'undefined') return null
   if (!token.accessToken) {
     router.push('/login')
   }
