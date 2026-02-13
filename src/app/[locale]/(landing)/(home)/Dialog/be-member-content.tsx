30e77e1e207729551fb2e5de3ab8a4fbf90b9c47
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-13 22:32:29 +0600

    fix :be member mobile response issue

--- src/app/[locale]/(landing)/(home)/Dialog/be-member-content.tsx
+++ src/app/[locale]/(landing)/(home)/Dialog/be-member-content.tsx
@@ -21,38 +21,38 @@ export const BeMemberContent = ({ onJoinClick }: BeMemberContentProps) => {
           loop
           muted
           playsInline
-          className="mt-10 size-80 rounded-full! object-cover md:mt-0 md:size-110"
+          className="mt-4 size-32 rounded-full! object-cover md:mt-0 md:size-110"
         />
       </div>
 
       {/* Right Side - Content */}
-      <div className="flex flex-1 flex-col items-center justify-center gap-12 px-6 py-8 md:px-12 md:py-16">
+      <div className="flex flex-1 flex-col items-center justify-center gap-4 px-6 py-8 md:gap-12 md:px-12 md:py-16">
         {/* Header */}
-        <div className="flex flex-col items-center gap-2 text-center">
-          <h2 className="font-leto text-4xl font-normal text-black">{t('title')}</h2>
-          <p className="max-w-87 text-base text-[#3E4259]">{t('subtitle')}</p>
+        <div className="flex flex-col items-center gap-1 text-center md:gap-2">
+          <h2 className="font-leto text-xl font-normal text-black md:text-4xl">{t('title')}</h2>
+          <p className="max-w-87 text-xs text-[#3E4259] md:text-base">{t('subtitle')}</p>
         </div>
 
         {/* Benefits Section */}
-        <div className="flex w-full flex-col gap-16">
+        <div className="flex w-full flex-col gap-4 md:gap-16">
           <div className="flex flex-col gap-4">
             {/* First Offer with Icon */}
             <div className="flex items-start gap-2">
               <div className="relative size-8 shrink-0">
                 <Image src={joinModalIcon} alt="Image" />
               </div>
-              <p className="text-base text-[#3e4259]">{t('firstOffer')}</p>
+              <p className="text-xs text-[#3e4259] md:text-base">{t('firstOffer')}</p>
             </div>
 
             {/* Additional Benefits */}
-            <p className="text-base text-[#3e4259]">{t('benefits')}</p>
+            <p className="text-xs text-[#3e4259] md:text-base">{t('benefits')}</p>
           </div>
 
           {/* CTA Button */}
           <button
             type="button"
             onClick={onJoinClick}
-            className="mx-auto flex h-12 w-fit cursor-pointer items-center justify-center rounded bg-black px-12 py-5 text-xl text-white transition-colors hover:bg-gray-900"
+            className="mx-auto flex h-12 w-fit cursor-pointer items-center justify-center rounded bg-black px-12 py-5 text-white transition-colors hover:bg-gray-900 lg:text-xl"
           >
             {t('cta')}
           </button>
